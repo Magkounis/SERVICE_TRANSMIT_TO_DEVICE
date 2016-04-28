95c2f13deee5f9ca70284d1c0fc97a7d3c79f4a6
Agentnick <nmagkoynis5@hotmail.com>
2016-04-28 13:41:42 +0300

    SERVICETOTRANSMITETODEVICE

---
+++ SERVICE_TRANSMIT_TO_DEVICE/PARAMETERS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SERVICE_TRANSMIT_TO_DEVICE
+{
+   public class PARAMETERS
+    {
+       public string Database { get; set; }
+       public string Server { get; set; }
+       public string Password { get; set; }
+       public string Remoteep { get; set; }
+       public string Dbuser { get; set; }
+       public string Port { get; set; }
+       public bool   Stop { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SERVICE_TRANSMIT_TO_DEVICE/Service1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+
+namespace SERVICE_TRANSMIT_TO_DEVICE
+{
+    public partial class Service1 : ServiceBase
+    {
+        
+        public Service1()
+        {
+            InitializeComponent();
+            Timer tmr = new Timer();
+            tmr.Interval = 2;
+            tmr.Start();
+            tmr.Elapsed += new ElapsedEventHandler(tmr_Elapsed);
+                
+        }
+
+        void tmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+           
+
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
